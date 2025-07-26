67be481d6c38a8e1bfddce6dd393e5545e016cd4
hot24-7@hotmail.com <isaac@Isaacs-MacBook-Air.local>
2025-07-26 10:28:49 -0500

    Initial themem push.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
-import { defineConfig } from 'astro/config'
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://iudexus.github.io',
   base: '/salemDynamicsWebsite',
-})
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
