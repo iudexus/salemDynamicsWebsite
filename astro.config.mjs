e7d2e713b70c2cc9bfc4615cbfaa47434758179f
hot24-7@hotmail.com <isaac@Isaacs-MacBook-Air.local>
2025-07-26 10:48:55 -0500

    adjusting astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://iudexus.github.io',
-  base: '/salemDynamicsWebsite',
+  site: 'https://salemdynamics.com',
+  base: '/',
   vite: {
     plugins: [tailwindcss()],
   },
